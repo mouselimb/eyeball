ba1492f1b5ee2bd73e843f08e8c1043f21118698
mouselimb <43717520+mouselimb@users.noreply.github.com>
2019-07-21 22:26:03 -0400

    Add files via upload

---
+++ eye.py
@@ -0,0 +1,249 @@
+################################################
+# Presented by Richard Jerez 07/10/2019        #
+# Program Name: c_hawks.py                     #
+# Week 2                                       #
+# Comments:                                    #
+#                                              #
+################################################
+
+from graphics import *
+
+################################################
+################################################
+################################################
+
+
+
+################################################
+################################################
+################################################
+
+def main():
+    win = GraphWin('c# Hawks Concept',450,275)
+
+    c1 = Circle(Point(225,137), 130)
+    c1.setFill(color_rgb(255, 237, 239))#pinkish white
+    c1.draw(win)
+
+    c2 = Circle(Point(225,137), 80)
+    c2.setFill(color_rgb(92, 52, 1))#brown
+    c2.draw(win)
+    
+    c3 = Circle(Point(225,137), 40)
+    c3.setFill('black')
+    c3.draw(win)
+    
+    oval2 = Oval(Point(250,110), Point(260,125))
+    oval2.setFill('white')
+    oval2.draw(win)
+    
+    oval2 = Oval(Point(275,85), Point(290,105))
+    oval2.setFill('white')
+    oval2.draw(win)
+
+#########################################################
+    
+    
+    #irisLine1 = Line(Point(225,177), Point(225,217))
+    #irisLine1.draw(win)
+    
+
+#########################################################
+
+    line1 = Line(Point(100,100), Point(115,115))
+    line1.setOutline('red')
+    line1.draw(win)
+    
+    line2 = Line(Point(115,115), Point(125,115))
+    line2.setOutline('red')
+    line2.draw(win)
+
+    line3 = Line(Point(115,115), Point(115,130))
+    line3.setOutline('red')
+    line3.draw(win)
+    
+    line4 = Line(Point(125,115), Point(130,125))
+    line4.setOutline('red')
+    line4.draw(win)
+    
+    line5 = Line(Point(115,130), Point(122,135))
+    line5.setOutline('red')
+    line5.draw(win)
+    
+    line6 = Line(Point(107,107), Point(111,95))
+    line6.setOutline('red')
+    line6.draw(win)
+    
+    line7 = Line(Point(111,95), Point(130,80))
+    line7.setOutline('red')
+    line7.draw(win)
+    
+    line8 = Line(Point(130,80), Point(138,72))
+    line8.setOutline('red')
+    line8.draw(win)
+    
+    line9 = Line(Point(134,76), Point(145,80))
+    line9.setOutline('red')
+    line9.draw(win)
+    
+#########################################################
+    
+    line10 = Line(Point(122,215), Point(129,215))
+    line10.setOutline('red')
+    line10.draw(win)
+    
+    line11 = Line(Point(129,215), Point(126,200))
+    line11.setOutline('red')
+    line11.draw(win)
+    
+    line12 = Line(Point(126,200), Point(120,190))
+    line12.setOutline('red')
+    line12.draw(win)
+    
+    line13 = Line(Point(120,190), Point(111,178))
+    line13.setOutline('red')
+    line13.draw(win)
+    
+    line14 = Line(Point(111,178), Point(116,170))
+    line14.setOutline('red')
+    line14.draw(win)
+    
+    line15 = Line(Point(120,190), Point(127,183))
+    line15.setOutline('red')
+    line15.draw(win)
+    
+    line16 = Line(Point(127,183), Point(136,188))
+    line16.setOutline('red')
+    line16.draw(win)
+    
+#########################################################
+
+    line17 = Line(Point(350,100), Point(340,115))
+    line17.setOutline('red')
+    line17.draw(win)
+    
+    line18 = Line(Point(340,115), Point(330,113))
+    line18.setOutline('red')
+    line18.draw(win)
+
+    line19 = Line(Point(330,113), Point(326,130))
+    line19.setOutline('red')
+    line19.draw(win)
+    
+    line20 = Line(Point(326,130), Point(320,128))
+    line20.setOutline('red')
+    line20.draw(win)
+    
+    line21 = Line(Point(330,113), Point(322,105))
+    line21.setOutline('red')
+    line21.draw(win)
+
+#########################################################
+       
+    line22 = Line(Point(328,215), Point(320,217))
+    line22.setOutline('red')
+    line22.draw(win)
+    
+    line23 = Line(Point(320,217), Point(326,188))
+    line23.setOutline('red')
+    line23.draw(win)
+    
+    line24 = Line(Point(326,188), Point(334,180))
+    line24.setOutline('red')
+    line24.draw(win)
+    
+    line25 = Line(Point(326,188), Point(320,175))
+    line25.setOutline('red')
+    line25.draw(win)
+    
+    line26 = Line(Point(320,217), Point(310,212))
+    line26.setOutline('red')
+    line26.draw(win)
+    
+    line27 = Line(Point(310,212), Point(302,220))
+    line27.setOutline('red')
+    line27.draw(win)
+    
+    line28 = Line(Point(310,212), Point(311,220))
+    line28.setOutline('red')
+    line28.draw(win)
+    
+#########################################################
+        
+    line28 = Line(Point(233,8), Point(227,16))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(227,16), Point(217,10))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(217,10), Point(209,15))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(227,16), Point(250,22))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(227,16), Point(230,30))
+    line28.setOutline('red')
+    line28.draw(win)
+        
+    line28 = Line(Point(250,22), Point(255,32))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(255,32), Point(249,40))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(230,30), Point(217,41))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(255,32), Point(249,40))
+    line28.setOutline('red')
+    line28.draw(win)
+    
+##################BOTTOM#################################
+            
+    line28 = Line(Point(233,267), Point(227,259))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(227,259), Point(217,257))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(217,257), Point(209,252))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(227,259), Point(250,245))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(227,259), Point(230,237))
+    line28.setOutline('red')
+    line28.draw(win)
+        
+    line28 = Line(Point(250,245), Point(255,235))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(255,235), Point(249,227))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(230,237), Point(217,226))
+    line28.setOutline('red')
+    line28.draw(win)
+            
+    line28 = Line(Point(255,235), Point(249,227))
+    line28.setOutline('red')
+    line28.draw(win)
+    
+#########################################################
+    
+main()
